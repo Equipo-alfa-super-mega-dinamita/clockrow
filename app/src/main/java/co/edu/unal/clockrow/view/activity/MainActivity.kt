f38e9675742fc39c0d3af5d5cefc93270a2d76f4
Edder <edhernandezfo@unal.edu.co>
2020-12-10 04:08:53 -0500

    Calendar

--- app/src/main/java/co/edu/unal/clockrow/view/activity/MainActivity.kt
+++ app/src/main/java/co/edu/unal/clockrow/view/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package co.edu.unal.clockrow.view.activity
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -11,19 +12,28 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
 import co.edu.unal.clockrow.R
+import co.edu.unal.clockrow.data.Task.TaskViewModel
+import co.edu.unal.clockrow.logic.Task
 import co.edu.unal.clockrow.view.activity.Task.TaskActivity
+import co.edu.unal.clockrow.view.components.TaskListAdapter
+import co.edu.unal.clockrow.viewmodel.ClockViewModel
+import com.github.sundeepk.compactcalendarview.domain.Event
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var buttonTask: ImageButton? = null
+class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     lateinit var toggle: ActionBarDrawerToggle
-
+    private var mTaskViewModel: TaskViewModel? = null
+    private var taskList: List<Task?>? = null
+    private  val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,7 +49,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         intent?.let {
             val source = it.getStringExtra("SOURCE")
-            when (source){
+            when (source) {
                 "SIGNUP" -> handleSignUp(it)
                 "LOGIN" -> handleLogin(it)
             }
@@ -54,13 +64,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val adBuilder = AdRequest.Builder().build()
         adView.loadAd(adBuilder)
 
-        if (getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE).getBoolean(getString(R.string.user_premium_shrpref), false)) {
+        if (getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).getBoolean(getString(R.string.user_premium_shrpref), false)) {
             adView.visibility = View.VISIBLE
-        }
-        else adView.visibility = View.GONE
+        } else adView.visibility = View.GONE
 
 
-        getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE).registerOnSharedPreferenceChangeListener { sharedPreferences, s ->
+        getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).registerOnSharedPreferenceChangeListener { sharedPreferences, s ->
             if (s == getString(R.string.user_premium_shrpref)) {
                 adView.visibility = if (sharedPreferences.getBoolean(s, false)) View.VISIBLE else View.GONE
             }
@@ -68,7 +77,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
-    private fun configDrawer(){
+    private fun configDrawer() {
 
 
         val drawerLayout = findViewById<DrawerLayout>(R.id.mainDrawerLayout)
@@ -82,7 +91,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
         navigationView.setNavigationItemSelectedListener {
-            when (it.itemId){
+            when (it.itemId) {
                 R.id.project_item -> sendToTasks()
                 R.id.premium_item -> sendToPremium()
                 R.id.login_item -> sendToLogin()
@@ -92,20 +101,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         val menu = navigationView.menu
-        if ( getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE).getString(getString(R.string.user_email_shrpref), null) != null )
-        {
+        if (getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).getString(getString(R.string.user_email_shrpref), null) != null) {
             menu.findItem(R.id.logout_item).isVisible = false
             menu.findItem(R.id.login_item).isVisible = true
-        }
-        else {
+        } else {
             menu.findItem(R.id.login_item).isVisible = false
             menu.findItem(R.id.logout_item).isVisible = true
         }
 
-        getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE).registerOnSharedPreferenceChangeListener { sharedPreferences, s ->
+        getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).registerOnSharedPreferenceChangeListener { sharedPreferences, s ->
 
             Log.i("PREFERENCIAS", "$s")
-            if (  sharedPreferences.getBoolean(getString(R.string.user_logged_shrpref), false )) {
+            if (sharedPreferences.getBoolean(getString(R.string.user_logged_shrpref), false)) {
                 if (sharedPreferences.getString(s, null) != null) {
                     Log.i("PREFERENCIAS", "Login habilitado")
                     menu.findItem(R.id.logout_item).isVisible = false
@@ -122,11 +129,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
-
     private fun logout() {
         FirebaseAuth.getInstance().signOut()
         onBackPressed()
-        val sharedPref= getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE).edit()
+        val sharedPref = getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).edit()
         sharedPref.clear()
         sharedPref.apply()
 
@@ -137,11 +143,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val email = intent.getStringExtra("EMAIL") ?: ""
         val username = intent.getStringExtra("USERNAME") ?: ""
         val provider = intent.getStringExtra("PROVIDER") ?: ""
-        Toast.makeText(applicationContext, "$username, Email $email, Provider $provider" , Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "$username, Email $email, Provider $provider", Toast.LENGTH_SHORT).show()
 
-        val sharedPref= getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE)
 
-        with (sharedPref.edit()) {
+        with(sharedPref.edit()) {
             putString(getString(R.string.user_email_shrpref), email)
             putString(getString(R.string.user_provider_shrpref), provider)
             putString(getString(R.string.user_username_shrpref), username)
@@ -155,11 +161,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val email = intent.getStringExtra("EMAIL") ?: ""
         val username = intent.getStringExtra("USERNAME") ?: ""
         val provider = intent.getStringExtra("PROVIDER") ?: ""
-        Toast.makeText(applicationContext, "$username, Email $email, Provider $provider" , Toast.LENGTH_SHORT).show()
+        Toast.makeText(applicationContext, "$username, Email $email, Provider $provider", Toast.LENGTH_SHORT).show()
 
-        val sharedPref= getSharedPreferences( getString(R.string.shrpref_file), Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE)
 
-        with (sharedPref.edit()) {
+        with(sharedPref.edit()) {
             putString(getString(R.string.user_email_shrpref), email)
             putString(getString(R.string.user_provider_shrpref), provider)
             putString(getString(R.string.user_username_shrpref), username)
@@ -201,10 +207,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun configView() {
+        mTaskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        compactcalendar_view.setUseThreeLetterAbbreviation(true)
+        val e: Event = Event(Color.GREEN,1607590310L,"info")
+        compactcalendar_view.addEvent(e)
+        /*
+        val adapter = TaskListAdapter(this)
+        taskList = emptyList()
+        mTaskViewModel!!.allTasks.observe(this, { tasks: List<Task?>? ->
+            adapter.setTasks(tasks)
+            tasks?.let {
+                taskList = it.toMutableList()
+                backupTasks()
+            }
+        })
 
-        buttonTask = findViewById(R.id.buttonTask)
-        buttonTask?.setOnClickListener(this)
-
+        for (task in taskList!!){
+            compactcalendar_view.addEvent(task?.let { createEvent(it) })
+        }
+*/
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -213,15 +234,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         } else super.onOptionsItemSelected(item)
     }
 
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.buttonTask -> {
-                Toast.makeText(applicationContext, "Hola buenas tardes", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 
     companion object {
         const val EXTRA_MESSAGE = "co.edu.unal.clockrow.MESSAGE"
+    }
+
+    private fun createEvent(task: Task): Event {
+        val name = task.name
+        val date = task.dueDate.time
+        Log.e(TAG, "$name $date")
+        return Event(Color.BLUE, date, name)
+    }
+
+    private fun backupTasks() {
+
+        val tasksSnapshot = taskList?.toMutableList()
+        val email = getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).getString(getString(R.string.user_email_shrpref), null)
+        val premium = getSharedPreferences(getString(R.string.shrpref_file), Context.MODE_PRIVATE).getBoolean(getString(R.string.user_premium_shrpref), false)
+
+
+
+        if (!premium) return //Premium feature.
+        if (email != null) {
+            Log.i("TASKS", "PIPO")
+            if (tasksSnapshot != null && tasksSnapshot.size > 0) {
+                for (item in tasksSnapshot) {
+                    Log.i("TASKS", item.toString())
+                    if (item != null) {
+                        db.collection("tasks").document("${email}_${item.id}").set(item)
+                    }
+                }
+            }
+        }
+
     }
 }
